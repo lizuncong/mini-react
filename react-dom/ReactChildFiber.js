98423adb7c83d2529b3db40547488f5aff5e1643
lizuncong <2900170896@qq.com>
2022-03-01 23:16:24 +0800

    feat: stash

--- react-dom/ReactChildFiber.js
+++ react-dom/ReactChildFiber.js
@@ -1,8 +1,24 @@
+import { REACT_ELEMENT_TYPE } from '@shared/ReactSymbols' 
 
 // shouldTrackSideEffects 是否要跟踪副作用
 function childReconciler(shouldTrackSideEffects){
-    function reconcileChildFibers(returnFiber, currentFirstChild, newChild){
 
+    function reconcileSingleElement(returnFiber, currentFirstChild, element){
+        const created = createFiberFromElement(element)
+        created.return = returnFiber
+        return created
+    }
+
+    // currentFirstChild 旧的fiber节点 newChild新的虚拟DOM
+    function reconcileChildFibers(returnFiber, currentFirstChild, newChild){
+        // 判断newChild是不是一个对象，是的话说明新的虚拟DOM只有一个React元素节点
+        const isObject = typeof newChild === 'obj' && newChild
+        if(isObject){
+            switch(newChild.$$typeof){
+                case REACT_ELEMENT_TYPE:
+                    return placeSingleChild(reconcileSingleElement(returnFiber, currentFirstChild, newChild))
+            }
+        }
     }
 
     return reconcileChildFibers
