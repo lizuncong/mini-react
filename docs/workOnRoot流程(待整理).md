5ff66ef1773dab6fbc35747016236e4124022d63
lizuncong <2900170896@qq.com>
2022-03-16 23:20:03 +0800

    feat: 收集副作用

--- docs/workOnRoot流程(待整理).md
+++ docs/workOnRoot流程(待整理).md
@@ -14,3 +14,12 @@ React源码中，如果一个节点只有一个子节点，而且这一个子节
 1.根据虚拟DOM创建新的fiber树
 
 2.把新的fiber树的内容同步到真实DOM中
+
+
+### collectEffectList
+- 为了避免遍历fiber树寻找有副作用的fiber节点，所以有了effectList
+- 在 fiber 树构建过程中，每当一个 fiber 节点的 flags 字段不为 NoFlags 时(代表需要执行副作用)，就把该fiber节点添加到
+effectList 中
+- effectList是一个单向链表，firstEffect代表链表中的第一个fiber节点，lastEffect代表链表中的最后一个fiber节点
+- fiber树的构建是深度优先的，也就是先向下构建子级fiber节点，子级节点构建完成后，再向上构建父级fiber节点，所以effectlist中总是子级fiber节点在前面
+- fiber节点构建完成的操作执行在 completeUnitOfWork方法，在这个方法里，不仅会对节点完成构建，也会将有flags的fiber节点添加到effectList中
--- react-dom/ReactFiberWorkLoop.js
+++ react-dom/ReactFiberWorkLoop.js
@@ -47,13 +47,46 @@ function performUnitOfWork(unitOfWork){
 }
 
 function completeUnitOfWork(unitOfWork){
-    let completeWork = unitOfWork
+    let completedWork = unitOfWork
     do {
-        const current = completeWork.alternate
-        const returnFiber = completeWork.return
+        const current = completedWork.alternate
+        const returnFiber = completedWork.return
+        // 完成此fiber对应的真实DOM节点创建和属性赋值的功能
         completeWork(current, returnFiber)
-    } while(completeWork)
+        // 收集当前fiber的副作用到父fiber上
+        collectEffectList(returnFiber, completedWork)
+        // 当前fiber完成后，查找下一个要构建的fiber
+    } while(completedWork)
 }
+
+function collectEffectList(returnFiber, completedWork){
+    // 如果父节点没有effectList，那就让父节点的firstEffect链表头指向当前节点
+    if(!returnFiber.firstEffect){
+        returnFiber.firstEffect = completedWork.firstEffect
+    }
+    if(completedWork.lastEffect){
+        // 并且父节点也有链表尾时
+        if(returnFiber.lastEffect){
+            // 将当前节点的effectList挂载到父节点的链表尾部
+            returnFiber.lastEffect.nextEffect = completedWork.firstEffect
+        }
+
+        returnFiber.lastEffect = completedWork.lastEffect
+    }
+    const flags = completedWork.flags
+    // 如果此完成的fiber有副作用，那么就需要添加到effectList里
+    if(flags){
+        // 如果父fiber有lastEffect的话，说明父fiber已经有effect链表
+        if(returnFiber.lastEffect){
+            returnFiber.lastEffect.nextEffect = completedWork
+        } else {
+            returnFiber.firstEffect = completedWork
+        }
+        returnFiber.lastEffect = completedWork
+    }
+}
+
+
 function markUpdateLaneFromFiberToRoot(sourceFiber){
     let node = sourceFiber
     let parent = node.return
