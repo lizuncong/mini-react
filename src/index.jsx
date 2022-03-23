18187e7f22ce5c230a5d21b6159f15a8867328a0
lizuncong <2900170896@qq.com>
2022-03-23 16:03:03 +0800

    feat: stash

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import './events'
-// import React from '@react'
-// import ReactDOM from '@react-dom'
+// import './events'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 // const element = (
@@ -15,3 +15,29 @@ import './events'
 // console.log(element)
 
 // ReactDOM.render(element, document.getElementById('root'))
+
+
+class ClickCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {count: 0};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        debugger
+        this.setState((state) => {
+            return {count: state.count + 1};
+        });
+    }
+    
+    componentDidUpdate() {}
+
+    render() {
+        return [
+            <button key="1" onClick={this.handleClick}>Update counter</button>,
+            <span key="2">{this.state.count}</span>
+        ]
+    }
+}
+ReactDOM.render(<ClickCounter />, document.getElementById('root'))
