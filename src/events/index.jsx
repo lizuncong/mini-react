611d01af101f7a8d7bf499cc45fe018a4b8e0a29
lizuncong <2900170896@qq.com>
2022-03-27 23:19:04 +0800

    feat: 源码中的合成事件

--- src/events/index.jsx
+++ src/events/index.jsx
@@ -1,7 +1,7 @@
-import React from './react'
-import ReactDOM from './react-dom'
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+// import React from './react'
+// import ReactDOM from './react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 const root = document.getElementById('root')
