8eb6809158da5bbe2d5442b9d424646fb99129b2
lizuncong <2900170896@qq.com>
2022-03-29 23:31:23 +0800

    feat: stash

--- react-dom/events/plugins/BeforeInputEventPlugin.js
+++ react-dom/events/plugins/BeforeInputEventPlugin.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
- */
-
 import type {DOMEventName} from '../../events/DOMEventNames';
 import type {Fiber} from 'react-reconciler/src/ReactInternalTypes';
 import type {AnyNativeEvent} from '../../events/PluginModuleType';
