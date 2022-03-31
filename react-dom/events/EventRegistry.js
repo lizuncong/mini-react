a8bbcdd4fa719f6cc2280e565942da9705390620
lizuncong <2900170896@qq.com>
2022-03-31 21:16:29 +0800

    feat: react dom源码拆分

--- react-dom/events/EventRegistry.js
+++ react-dom/events/EventRegistry.js
@@ -1,52 +1,18 @@
+// 用于手机所有的原生事件名称，最后根据这个集合注册原生事件
 export const allNativeEvents = new Set();
 
-
 /**
- * Mapping from registration name to event name
+ * 注册名称和事件名称映射关系
  */
 export const registrationNameDependencies = {};
 
-/**
- * Mapping from lowercase registration names to the properly cased version,
- * used to warn in the case of missing event handlers. Available
- * only in __DEV__.
- * @type {Object}
- */
-export const possibleRegistrationNames = __DEV__ ? {} : (null: any);
-// Trust the developer to only use possibleRegistrationNames in __DEV__
-
-export function registerTwoPhaseEvent(
-  registrationName: string,
-  dependencies: Array<DOMEventName>,
-): void {
+export function registerTwoPhaseEvent(registrationName, dependencies){
   registerDirectEvent(registrationName, dependencies);
   registerDirectEvent(registrationName + 'Capture', dependencies);
 }
 
-export function registerDirectEvent(
-  registrationName: string,
-  dependencies: Array<DOMEventName>,
-) {
-  if (__DEV__) {
-    if (registrationNameDependencies[registrationName]) {
-      console.error(
-        'EventRegistry: More than one plugin attempted to publish the same ' +
-          'registration name, `%s`.',
-        registrationName,
-      );
-    }
-  }
-
+export function registerDirectEvent(registrationName, dependencies) {
   registrationNameDependencies[registrationName] = dependencies;
-
-  if (__DEV__) {
-    const lowerCasedName = registrationName.toLowerCase();
-    possibleRegistrationNames[lowerCasedName] = registrationName;
-
-    if (registrationName === 'onDoubleClick') {
-      possibleRegistrationNames.ondblclick = registrationName;
-    }
-  }
 
   for (let i = 0; i < dependencies.length; i++) {
     allNativeEvents.add(dependencies[i]);
