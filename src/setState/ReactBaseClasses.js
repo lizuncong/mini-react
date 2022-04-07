b349579ab3b605f79975363500b8b097810aebb0
lizuncong <2900170896@qq.com>
2022-04-07 23:01:15 +0800

    feat: stash

--- src/setState/ReactBaseClasses.js
+++ src/setState/ReactBaseClasses.js
@@ -1,5 +1,35 @@
+import { SyncLane } from './ReactFiberLane'
+const classComponentUpdater = {
+    enqueueSetState(inst, payload){
+        const fiber = get(inst)
+        const eventTime = requestEventTime()
+        const lane = requestUpdateLane(fiber)
+        const update = createUpdate() // 创建新的更新对象
+    }
+}
+
+function createUpdate(){
+    
+}
+
+function requestEventTime(){
+    return performance.now()
+}
+function get(inst){
+    return inst._reactInternal
+}
+
+function requestUpdateLane(fiber){
+    return SyncLane
+}
+
+
 class Component{
     constructor(){
         this.updater = classComponentUpdater
+    }
+
+    setState(partialState){
+        this.updater.enqueueSetState(this, partialState)
     }
 }
---
+++ src/setState/ReactFiberLane.js
@@ -0,0 +1,5 @@
+export const NoLanes: Lanes = /*                        */ 0b0000000000000000000000000000000;
+export const NoLane: Lane = /*                          */ 0b0000000000000000000000000000000;
+
+export const SyncLane: Lane = /*                        */ 0b0000000000000000000000000000001;
+export const SyncBatchedLane: Lane = /*                 */ 0b0000000000000000000000000000010;
