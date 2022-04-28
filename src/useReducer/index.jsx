2a22203fc065e6b4f91cc280b837c279e9c32a71
lizuncong <2900170896@qq.com>
2022-04-28 23:41:04 +0800

    feat: stash

--- src/useReducer/index.jsx
+++ src/useReducer/index.jsx
@@ -7,18 +7,18 @@ function reducer(state, action) {
 
 const Counter = () => {
   const [count, setCount] = useReducer(reducer, 0);
-  console.log('render======', count)
+  console.log("render======", count);
   return (
     <button
       onClick={() => {
-        debugger;
-        setCount(1);
-        setCount(2);
-        // setTimeout(() => {
-        //   debugger;
-        //   setCount(1);
-        //   setCount(2);
-        // }, 0);
+        // debugger;
+        // setCount(1);
+        // setCount(2);
+        setTimeout(() => {
+          debugger;
+          setCount(1);
+          setCount(2);
+        }, 0);
       }}
     >
       {count}
