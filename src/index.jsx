a513fdd9557ced923ed004e4ba03fc72486ef1bc
lizuncong <2900170896@qq.com>
2022-05-02 11:25:35 +0800

    doc: readme

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,25 @@
-import "./useReducer/index.jsx";
-// import './setState/index.jsx'
+// import "./setState/index.jsx";
 // import './workLoop'
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+class Counter extends Component {
+  constructor(props) {
+    debugger;
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+  handleClick = (event) => {
+    debugger;
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  render() {
+    console.log("render===", this.state);
+    return <button onClick={this.handleClick}>{this.state.number}</button>;
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
