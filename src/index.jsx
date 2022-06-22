3079f4ce5f86a2c3d4c10f2e1cb2059747bf6639
lizuncong <2900170896@qq.com>
2022-06-22 18:51:16 +0800

    feat: stash

--- src/index.jsx
+++ src/index.jsx
@@ -18,32 +18,42 @@ class Home extends React.Component {
   render() {
     const { step } = this.state;
     return (
-      <div
-        style={{ height: "100px" }}
-        id={"A-" + step}
-        onClick={this.handleClick}
-      >
-        <div id={"B-" + step}>
-          <div id={"D"}>
-            <div id={"F-" + step}></div>
-            {!(step % 2) && <div id={"K-" + step}></div>}
-          </div>
-          <div id={"E-" + step}>
-            <div id={"G-" + step}></div>
-            {!!(step % 2) && <div id={"H-" + step}></div>}
-            {!(step % 2) && <div id={"I-" + step}></div>}
-            <div id={"J-" + step}></div>
-          </div>
-        </div>
-        <div id={"C-" + step}></div>
-        <div id="L">
-          {!(step % 2) && <div key="M" id={"M-" + step}></div>}
-          <div key="N" id={"N-" + step}></div>
-          {!!(step % 2) && <div key="M" id={"M-" + step}></div>}
-        </div>
+      <div style={{ height: "100px" }} id={"A" + step} onClick={this.handleClick}>
+        <div id={"B" + step}></div>
+        {!(step % 2) && <div id="C"></div>}
+        <div id={"D" + step}></div>
       </div>
     );
   }
+  // render() {
+  //   const { step } = this.state;
+  //   return (
+  //     <div
+  //       style={{ height: "100px" }}
+  //       id={"A-" + step}
+  //       onClick={this.handleClick}
+  //     >
+  //       <div id={"B-" + step}>
+  //         <div id={"D"}>
+  //           <div id={"F-" + step}></div>
+  //           {!(step % 2) && <div id={"K-" + step}></div>}
+  //         </div>
+  //         <div id={"E-" + step}>
+  //           <div id={"G-" + step}></div>
+  //           {!!(step % 2) && <div id={"H-" + step}></div>}
+  //           {!(step % 2) && <div id={"I-" + step}></div>}
+  //           <div id={"J-" + step}></div>
+  //         </div>
+  //       </div>
+  //       <div id={"C-" + step}></div>
+  //       <div id="L">
+  //         {!(step % 2) && <div key="M" id={"M-" + step}></div>}
+  //         <div key="N" id={"N-" + step}></div>
+  //         {!!(step % 2) && <div key="M" id={"M-" + step}></div>}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 }
 
 ReactDOM.render(<Home />, document.getElementById("root"));
