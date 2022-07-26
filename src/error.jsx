0d9f2ec7c13e0165df4d623e957b7be7ac739f84
lizuncong <2900170896@qq.com>
2022-07-26 13:17:50 +0800

    fix: demo

---
+++ src/error.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // 更新 state 使下一次渲染能够显示降级后的 UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // 你同样可以将错误日志上报给服务器
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { children } = this.props;
+    if (this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return children;
+  }
+}
+
+
+export default ErrorBoundary
--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./counter";
-class ErrorBoundary extends React.Component {
+import ErrorBoundary from './error'
+
+class Home extends React.Component{
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
-    // 更新 state 使下一次渲染能够显示降级后的 UI
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    // 你同样可以将错误日志上报给服务器
-    console.log(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // 你可以自定义降级后的 UI 并渲染
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return <Counter />;
+  render(){
+    return  <div><Counter /></div>
   }
 }
-ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
