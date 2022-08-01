85a7d4c82243783fa25f45258372eca28e89b954
lizuncong <2900170896@qq.com>
2022-08-01 23:29:45 +0800

    feat: stash

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useLayoutEffect, useEffect } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  // useLayoutEffect(() => {
+  const [count, setCount] = useState({ a: 1 });
+  // useEffect(() => {
   //   console.log(aaadd);
   // }, []);
   return (
     <div
       onClick={() => {
-        console.log("after setCount");
-        setCount({ a: 1 });
+        // console.log("after setCount");
+        // setCount({ a: 1 });
         // Promise.resolve()
         //   .then(() => {
         //     // debugger;
