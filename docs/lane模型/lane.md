b00d6cde45a233f5528f22b247c8be9e36bac52b
lizuncong <2900170896@qq.com>
2022-08-25 23:24:54 +0800

    feat: stash

--- docs/lane模型/lane.md
+++ docs/lane模型/lane.md
@@ -1,15 +1,11 @@
->TODO
-搬砖中
+> TODO
+> 搬砖中
 
+## 参考链接
+
+- [https://dev.to/okmttdhr/what-is-lane-in-react-4np7](- https://dev.to/okmttdhr/what-is-lane-in-react-4np7)
 - [scheduling-in-react](https://philippspiess.com/scheduling-in-react/)
-- [lane的pr](https://github.com/facebook/react/pull/18796)
-- [what-is-lane-in-react-4np7](https://dev.to/okmttdhr/what-is-lane-in-react-4np7)
-- [https://qdmana.com/2022/115/202204291637158194.html](https://qdmana.com/2022/115/202204291637158194.html)
+- [lane 的 pr](https://github.com/facebook/react/pull/18796)
 - [https://qdmana.com/2022/115/202204291637158194.html](https://qdmana.com/2022/115/202204291637158194.html)
 - [https://www.youtube.com/c/JSerJSer](https://www.youtube.com/c/JSerJSer)
 - [https://segmentfault.com/a/1190000039134817](https://segmentfault.com/a/1190000039134817)
-
-- https://nanyang.io/post/deep-dive-into-react-fiber
-- https://dev.to/okmttdhr/what-is-lane-in-react-4np7
-
-- https://github.com/zepang/web-clipper-articles/issues/27
--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,21 @@
-import React, {
-  useContext,
-  useRef,
-  useState,
-  useImperativeHandle,
-  memo,
-} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
-const FunctionCounter = (props, ref) => {
-  const createInst = () => ({
-    focus: () => {
-      console.log("focus...");
-    },
-  });
-  useImperativeHandle(ref, createInst);
-  return <div>{`计数器：${props.count}`}</div>;
-};
-
-const ForwardRefCounter = React.forwardRef(FunctionCounter);
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.domRef = null; // React.createRef
     this.state = {
-      count: 0,
+      text: "",
     };
-  }
-  componentDidMount() {
-    console.log("dom节点ref：this.domRef", this.domRef);
   }
 
   render() {
     return (
-      <>
-        <ForwardRefCounter
-          ref={(el) => (this.domRef = el)}
-          count={this.state.count}
-        />
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          {this.state.count}
-        </button>
-      </>
+      <input
+        type="text"
+        value={this.state.text}
+        onChange={(e) => this.setState({ text: e.target.value })}
+      />
     );
   }
 }
